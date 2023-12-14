9f2c6ea960e8f543c2f6e3164cba6e2c6af1f194
ricky <39970880+torickyto@users.noreply.github.com>
2023-12-13 18:48:35 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyGoodnessHarold;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyGoodnessHarold;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
