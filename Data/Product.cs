7b4e8b0046f4827bf5fce3690e5296263ba68d0f
Logan Hoppen <Logan.Hoppen@edu.sait.ca>
2023-12-14 16:05:54 -0700

    logans ui for product management and working delete buttons

--- Data/Product.cs
+++ Data/Product.cs
@@ -29,13 +29,13 @@ namespace MyGoodnessHarold.Data
                 Server = "localhost",
                 Database = "harold",
                 UserID = "root",
-                Password = "andromon"
+                Password = "password"
             };
 
             using (var connection = new MySqlConnection(builder.ConnectionString))
             {
                 connection.Open();
-                string sql = "SELECT * FROM products";
+                string sql = "SELECT p.*, c.CategoryName FROM products p JOIN Categories c ON p.CategoryID = c.CategoryID";
                 MySqlCommand command = new MySqlCommand(sql, connection);
                 MySqlDataReader reader = command.ExecuteReader();
 
@@ -46,7 +46,7 @@ namespace MyGoodnessHarold.Data
                         reader.GetString("ProductName"),
                         reader.GetDecimal("Price"),
                         reader.GetInt32("StockQuantity"),
-                        reader.GetString("Category")
+                        reader.GetString("CategoryName")
                     ));
                 }
             }
@@ -57,6 +57,55 @@ namespace MyGoodnessHarold.Data
         public override string ToString()
         {
             return $"{ProductID}, {ProductName}, {Price:C}, {StockQuantity}, {Category}";
+        }
+        public static void DeleteProduct(int productId)
+        {
+            var builder = new MySqlConnectionStringBuilder()
+            {
+                Server = "localhost",
+                Database = "harold",
+                UserID = "root",
+                Password = "password"
+            };
+
+            using (var connection = new MySqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+                // Example SQL command to delete a product by ProductID
+                string sql = "DELETE FROM products WHERE ProductID = @ProductId";
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductId", productId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public static void SaveProduct(string productName, decimal price, int stockQuantity, int categoryId)
+        {
+            var builder = new MySqlConnectionStringBuilder()
+            {
+                Server = "localhost",
+                Database = "harold",
+                UserID = "root",
+                Password = "password"
+            };
+
+            using (var connection = new MySqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+
+               
+                string sql = "INSERT INTO products (ProductName, Price, StockQuantity, CategoryID) VALUES (@ProductName, @Price, @StockQuantity, @CategoryId)";
+                using (var command = new MySqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductName", productName);
+                    command.Parameters.AddWithValue("@Price", price);
+                    command.Parameters.AddWithValue("@StockQuantity", stockQuantity);
+                    command.Parameters.AddWithValue("@CategoryId", categoryId);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
