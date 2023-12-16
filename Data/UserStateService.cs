c6bc42e597b245d1dec5ff847e09728805dbf7bd
Logan Hoppen <Logan.Hoppen@edu.sait.ca>
2023-12-15 19:09:43 -0700

    dontbreakpls

--- Data/UserStateService.cs
+++ Data/UserStateService.cs
@@ -6,11 +6,7 @@ namespace MyGoodnessHarold.Data
     {
         public string FirstName { get; private set; }
 
-<<<<<<< HEAD
         public event Action OnUserStateChanged;
-=======
-        public event Action OnUserStateChanged;
->>>>>>> 210a73951002691b36dacfff4228920dbd255187
 
         public void SetCurrentUser(string firstName)
         {
