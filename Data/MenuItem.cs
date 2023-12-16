e36c6297f90f849e5934c799887a45fa1b055d99
MushysWebs <141169926+MushysWebs@users.noreply.github.com>
2023-12-15 19:25:49 -0700

    Comments

--- Data/MenuItem.cs
+++ Data/MenuItem.cs
@@ -1,4 +1,17 @@
 namespace MyGoodnessHarold.Data
+/*
+Team members: Grady Spurrill, Ricky To, Logan Hoppen
+POS Menu System for "My Goodness Harold" Kitchen
+
+This POS (Point of Sale) menu system is designed to facilitate the ordering process for a kitchen environment.
+It displays a list of food items retrieved from a database, allowing kitchen staff or customers to place orders.
+Users are greeted by name and can view the current time. They can add items to their order by interacting with the menu,
+which updates a running total including tax. The system supports item selection, quantity updates, and order finalization.
+Once an order is finalized, a message is displayed, and there is an option to print the order. The interface is intuitive,
+ensuring a smooth ordering experience.
+*/
+
+    //Getters and setters and constructor for MenuItems
 {
     public class MenuItem
     {
--- Data/ProductService.cs
+++ Data/ProductService.cs
@@ -1,38 +1,54 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/*
+Team members: Grady Spurrill, Ricky To, Logan Hoppen
+POS Menu System for "My Goodness Harold" Kitchen
+
+This POS (Point of Sale) menu system is designed to facilitate the ordering process for a kitchen environment.
+It displays a list of food items retrieved from a database, allowing kitchen staff or customers to place orders.
+Users are greeted by name and can view the current time. They can add items to their order by interacting with the menu,
+which updates a running total including tax. The system supports item selection, quantity updates, and order finalization.
+Once an order is finalized, a message is displayed, and there is an option to print the order. The interface is intuitive,
+ensuring a smooth ordering experience.
+*/
+
+//this class is for managing the products we sell
 namespace MyGoodnessHarold.Data
 {
     public class ProductService
     {
+        //list of products
         public List<Product> Products { get; private set; }
+        //list of menu items
         public List<MenuItem> SelectedItems { get; private set; } = new List<MenuItem>();
-
+        
         public List<string> Categories { get; private set; }
 
+        //create a new productservice
         public ProductService()
         {
             Products = Product.GetAllProducts();
             Categories = FetchCategories();
         }
-
+        //list of categoires
         private List<string> FetchCategories()
         {
             return new List<string> { "All", "Mains", "Appetizers", "Desserts" };
         }
-
+        //method to delete a product from order
         public void DeleteProduct(int productId)
         {
             Product.DeleteProduct(productId);
             Products = Product.GetAllProducts();
         }
-
+        //adds new product to the menu
         public void AddNewProduct(string productName, decimal price, int stockQuantity, int categoryId)
         {
             Product.SaveProduct(productName, price, stockQuantity, categoryId);
             Products = Product.GetAllProducts();
         }
-
+        //adds a new item the the order
         public void AddItem(Product product)
         {
             var existingItem = SelectedItems.FirstOrDefault(i => i.Name == product.ProductName);
@@ -45,7 +61,7 @@ namespace MyGoodnessHarold.Data
                 SelectedItems.Add(new MenuItem(product.ProductName, product.Price, 1));
             }
         }
-
+        //removes an item from the order
         public void RemoveItem(MenuItem item)
         {
             var existingItem = SelectedItems.FirstOrDefault(i => i.Name == item.Name);
@@ -58,12 +74,13 @@ namespace MyGoodnessHarold.Data
                 }
             }
         }
-
+        //updates quantity in database
         public void UpdateProductStock(Product product, int quantity)
         {
             Product.UpdateStockQuantity(product.ProductID, quantity);
         }
 
+        //finalize the order
         public string ProcessOrder()
         {
             foreach (var item in SelectedItems)
@@ -82,7 +99,7 @@ namespace MyGoodnessHarold.Data
         }
 
 
-
+        //calculate the total of the entire thing with taxs
         public decimal GetTotal()
         {
             return SelectedItems.Sum(item => item.Price * item.Quantity);
