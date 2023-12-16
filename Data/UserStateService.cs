3aa0df82a618bf880402420c7812858d46d5d862
MushysWebs <141169926+MushysWebs@users.noreply.github.com>
2023-12-15 18:35:27 -0700

    Wow

--- Data/UserStateService.cs
+++ Data/UserStateService.cs
@@ -1,16 +1,14 @@
 using System;
 
+using System;
+
 namespace MyGoodnessHarold.Data
 {
     public class UserStateService
     {
         public string FirstName { get; private set; }
 
-<<<<<<< HEAD
         public event Action OnUserStateChanged;
-=======
-        public event Action OnUserStateChanged;
->>>>>>> 210a73951002691b36dacfff4228920dbd255187
 
         public void SetCurrentUser(string firstName)
         {
